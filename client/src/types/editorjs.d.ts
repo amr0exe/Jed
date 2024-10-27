ef75ec17b14a739cf207494c24a1d3ee2bffb885
amr0exe <zyklen908@gmail.com>
2024-10-28 00:11:45 +0545

    Add: types for editorjsHeaders

---
+++ client/src/types/editorjs.d.ts
@@ -0,0 +1,6 @@
+declare module "@editorjs/headers" {
+    import { BlockTool } from "@editorjs/editorjs"
+    export default class Header implements BlockTool {
+        constructor(options: any)
+    }
+}
